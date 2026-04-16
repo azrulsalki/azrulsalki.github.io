7ecf4b25b1f76ef493cfbf09ee59eef5da1ad174
azrulsalki <arshad8338@gmail.com>
2026-04-16 11:24:43 +0800

    Changes to Portfolio
    cleaner looks.
    overhaul.
    update portfolio - 16/4/2026

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  
+    const elements = document.querySelectorAll(".hidden");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  elements.forEach((el) => observer.observe(el));
+
+});
+
+{const text = "IT Support Specialist | System Engineer";
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 50);
+  }
+}
+
+type();}
+
+{const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
+
+// Toggle theme
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Save preference
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});}
